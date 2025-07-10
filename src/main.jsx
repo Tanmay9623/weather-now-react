86a81d2653343e61e2464365ab9dfaca5173b301
Tanmay9623 <Tanmay9623bari@gmail.com>
2025-07-10 10:51:30 +0530

    Initial commit of React Weather App

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './css/style.css';
+import './css/search.css';
+import './css/world.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
